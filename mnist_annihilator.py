3f25154ecaee74135e89290030e51f7558b98b8f
baptisteeeeeeee <baptiste.juran@student-cs.fr>
2023-07-26 19:49:01 +0200

    refactoring

--- mnist_annihilator.py
+++ mnist_annihilator.py
@@ -17,20 +17,21 @@ def mnist_annihilator():
 
     x = tf.keras.layers.DepthwiseConv2D(kernel_size=(3, 3), padding='same')(x)
     x = tf.keras.layers.Activation('relu')(x)
-    x = tf.keras.layers.Conv2D(filters=8, kernel_size=(1, 1), activation='relu', padding='same')(x)
+    x = tf.keras.layers.Conv2D(filters=8, kernel_size=(1, 1), padding='same')(x)
+    x = tf.keras.layers.Activation('relu')(x)
 
     x = tf.keras.layers.Flatten()(x)
     x = tf.keras.layers.Dense(10)(x)
 
     model = tf.keras.models.Model(input, x)
     model.summary()
-    model.compile(optimizer='adam', loss=tf.keras.losses.sparse_categorical_crossentropy, metrics=['accuracy'])
+    model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
 
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
     x_train = tf.expand_dims(x_train, axis=-1)
     x_test = tf.expand_dims(x_test, axis=-1)
 
-    model.fit(x_train, y_train, epochs=60, batch_size=200, validation_split=0.01)
+    model.fit(x_train, y_train, epochs=30, batch_size=200, validation_split=0.01)
 
 
 if __name__ == "__main__":
