c4471b59b0b0b3ddff5fe2c409001df2b3937b07
baptisteeeeeeee <bjuran2001@gmail.com>
2023-12-21 18:41:00 +0100

    [param] swish activation function

--- mnist_annihilator.py
+++ mnist_annihilator.py
@@ -4,21 +4,21 @@ import tensorflow as tf
 def mnist_annihilator():
     input = tf.keras.layers.Input((28, 28, 1))
     x = tf.keras.layers.Conv2D(filters=32, kernel_size=(2, 2), dilation_rate=(2, 2), padding='same')(input)
-    x = tf.keras.layers.Activation('leaky_relu')(x)
+    x = tf.keras.layers.Activation('swish')(x)
     x = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(x)
 
     x = tf.keras.layers.DepthwiseConv2D(kernel_size=(2, 2), dilation_rate=(2, 2), padding='same')(x)
-    x = tf.keras.layers.Activation('leaky_relu')(x)
+    x = tf.keras.layers.Activation('swish')(x)
     x = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(x)
 
     x = tf.keras.layers.DepthwiseConv2D(kernel_size=(2, 2), dilation_rate=(2, 2), padding='same')(x)
-    x = tf.keras.layers.Activation('leaky_relu')(x)
+    x = tf.keras.layers.Activation('swish')(x)
     x = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(x)
 
     x = tf.keras.layers.DepthwiseConv2D(kernel_size=(2, 2), dilation_rate=(2, 2), padding='same')(x)
-    x = tf.keras.layers.Activation('leaky_relu')(x)
+    x = tf.keras.layers.Activation('swish')(x)
     x = tf.keras.layers.Conv2D(filters=8, kernel_size=(1, 1), padding='same')(x)
-    x = tf.keras.layers.Activation('leaky_relu')(x)
+    x = tf.keras.layers.Activation('swish')(x)
 
     x = tf.keras.layers.Flatten()(x)
     x = tf.keras.layers.Dense(10)(x)
@@ -31,7 +31,7 @@ def mnist_annihilator():
     x_train = tf.expand_dims(x_train, axis=-1)
     x_test = tf.expand_dims(x_test, axis=-1)
 
-    model.fit(x_train, y_train, epochs=100, batch_size=200, validation_split=0.01)
+    model.fit(x_train, y_train, epochs=200, batch_size=200, validation_split=0.01)
 
     model.save("annihilator.h5")
 
